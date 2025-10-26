0c5dfef1f425e65eacd8100eaa7eb839304a4188
Gammy98 <dnqorgh0410@naver.com>
2025-10-26 13:34:40 +0900

    자동차 이름 입력 기능 구현

---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,10 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+
+public class RacingGameController {
+
+    public void start(){
+        String carNameInput = InputView.readCarName();
+    }
+}
