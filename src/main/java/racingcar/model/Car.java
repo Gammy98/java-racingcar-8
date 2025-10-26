4d57c52c22566b5a2dd1f12b9d3641c5344a57b8
Gammy98 <dnqorgh0410@naver.com>
2025-10-26 19:25:08 +0900

    feat : 핵심로직 구현을 위한 TDD 검증

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,9 +1,18 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Random;
+
 public class Car {
     private static final int MAX_NAME_LENGTH = 5;
     private static final String ERROR_MESSAGE = "ERROR : " ;
+    private static final int MOVE_FORWARD_BOUNDARY = 4;
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+
     private final String name;
+    private int position = 0;
 
     public Car(String name){
         validateName(name);
@@ -27,8 +36,25 @@ public class Car {
         return name.trim().length() > MAX_NAME_LENGTH;
     }
 
+    //test용이다 (random사용 x)
+    void move(int number) {
+        if (number >= MOVE_FORWARD_BOUNDARY) {
+            this.position++;
+        }
+    }
+
+    //실제 구현에 사용되는 코드
+    public void move(){
+        int randomNum = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+        move(randomNum);
+    }
+
     public String getName(){
         return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
 
--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
 
@@ -36,5 +35,30 @@ public class CarTest {
         assertThatThrownBy(() -> new Car(blankName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("ERROR : 자동차 이름은 비어 있을 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("자동차는 생성 시 위치가 0에서 시작")
+    void car_initial_position(){
+        Car car = new Car("Pobi");
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @DisplayName("입력값이 4 이상일 경우 자동차는 1칸 전진한다.")
+    void car_moves_if_number_is_4_or_more(int number) {
+        Car car = new Car("pobi");
+        car.move(number);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    @DisplayName("입력값이 4 미만일 경우 자동차는 멈춘다.")
+    void car_stops_if_number_is_3_or_less(int number) {
+        Car car = new Car("pobi");
+        car.move(number); // <-- 숫자를 직접 주입
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
